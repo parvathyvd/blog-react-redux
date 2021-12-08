5778496deb8e9782822b91401d86cc4cf86c7f61
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2021-12-08 12:36:39 -0800

    Changed the fetch user to use memoize to reduce the api call

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+
+import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
+
+ const reducers = combineReducers({
+  posts: postsReducer,
+  users: usersReducer
+});
+
+export default reducers
